55784a78bd304048f9258c911974a8c69e593c04
Davi Oliveira <DaviAlvesOliveira2006@Outlook.com>
2025-11-25 18:53:06 -0700

    Fixed authorization error

--- mcapi/authorization/routes.js
+++ mcapi/authorization/routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const AuthController = require('./controller');
 router.post('/signup', AuthController.register);
-module.exports = router;
-
 router.post('/login', AuthController.login);
+module.exports = router;
