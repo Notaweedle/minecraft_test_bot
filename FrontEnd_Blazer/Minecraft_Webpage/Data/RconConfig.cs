5a36a80091814d5b3236d594bd456dc7693b88d8
PrincessIora <pjgatehouse@gmail.com>
2025-12-09 19:32:04 -0700

    Updated Port to have constraint, serverPort: { type: 'number', minimum: 1, maximum: 65535 },

--- FrontEnd_Blazer/Minecraft_Webpage/Data/RconConfig.cs
+++ FrontEnd_Blazer/Minecraft_Webpage/Data/RconConfig.cs
@@ -1,11 +1,18 @@
-﻿namespace Minecraft_Webpage.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Minecraft_Webpage.Data
 {
     public class RconConfig
     {
         [System.ComponentModel.DataAnnotations.Key]
         public int Id { get; set; }          // Primary Key
+
+        [Required]
         public string ServerIp { get; set; }
+        [Required]
+        [Range(1, 65535, ErrorMessage = "Port must be between 1 and 65535.")]
         public int ServerPort { get; set; }
+        [Required]
         public string ServerPassword { get; set; }
     }
 }
---
+++ FrontEnd_Blazer/Minecraft_Webpage/Migrations/20251210023012_AddPortRangeConstraint.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minecraft_Webpage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPortRangeConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
