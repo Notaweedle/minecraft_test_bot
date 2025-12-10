50c3597573cc940126183d8416b3f1b404e85524
PrincessIora <pjgatehouse@gmail.com>
2025-12-09 19:12:50 -0700

    Database

---
+++ FrontEnd_Blazer/Minecraft_Webpage/Data/RconConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Minecraft_Webpage.Data
+{
+    public class RconConfig
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int Id { get; set; }          // Primary Key
+        public string ServerIp { get; set; }
+        public int ServerPort { get; set; }
+        public string ServerPassword { get; set; }
+    }
+}
